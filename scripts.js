6c371487691d6aa5b093a8dfd7ebfda49dda6b64
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-02-06 03:03:02 -0300

    Finalização

--- scripts.js
+++ scripts.js
@@ -113,9 +113,9 @@ const DOM = {
 
 const Utils = {
     formatAmount(value){
-        value = Number(value.replace(/\,\./g, "")) * 100
 
-        return value
+        value = value * 100
+        return Math.round(value)
     },
 
     formatDate(date){
